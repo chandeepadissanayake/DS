8f2e88eae03339613669b7850b38f75b5609d6a2
VishalThenuwara <sudharaka12345@gmail.com>
2020-04-23 20:47:22 +0530

    commit 2 check

--- REST_API_DS/src/main/java/com/example/demo/model/Sensor.java
+++ REST_API_DS/src/main/java/com/example/demo/model/Sensor.java
@@ -3,6 +3,8 @@ package com.example.demo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+//sensor class
+
 @Document
 public class Sensor {
 	@Id
